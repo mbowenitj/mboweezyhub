48f933487e5108db85ba02b66caa2fd3f7d156fd
Tshepo Mboweni <tshepo.mboweni@rank.com>
2025-06-24 12:43:54 +0200

    test

--- backend/next.config.mjs
+++ backend/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true
+  // No need to manually expose env variables here
+  // Next.js automatically makes .env* files available
 };
 
 export default nextConfig;
